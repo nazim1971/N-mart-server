c9021a2334cc83aa57782970376444b561c8ab29
nazim1971 <md.nazimuddinaj@gmail.com>
2024-08-16 15:10:40 +0600

    update cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174' , 'https://n-mart-43656.web.app'],
     credentials: true,
     optionSuccessStatus: 200,
   }
